7a16f0b0278de5a66fb4d2888e3796e7b92a1519
Andrew Daly <dalyandrewd@gmail.com>
2015-10-05 22:41:18 +1300

    added Wall.java

---
+++ src/Model/Wall.java
@@ -0,0 +1,23 @@
+package Model;
+
+import View.ID;
+
+public class Wall extends Actor {
+
+	public Wall(ID id, Position position, char ascii, boolean collidable,
+			boolean drawable, int boundingBoxSize) {
+		super(id, position, ascii, true, drawable, boundingBoxSize);
+
+	}
+
+	@Override
+	public void tick() {
+	}
+
+
+	@Override
+	public void setCollidable(boolean collidable) {
+        this.collidable = true;
+    }
+
+}
